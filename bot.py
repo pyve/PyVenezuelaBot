eb20f19ccd1f10ecfff222e12f4ae2f77c0b0dd9
Nhomar Hernández <nhomar@vauxoo.com>
2016-03-16 23:41:24 -0600

    [IMP] Ahora desde environment y desde archivo el token. [IMP] algunos lints resueltos en comparación de tipos con None debe ser usado en is. [FIX] Lineas muy largas.

--- bot.py
+++ bot.py
@@ -11,12 +11,21 @@ from telebot import util
 from lxml.etree import fromstring
 from lxml.cssselect import CSSSelector
 import re
+import os
 
 MAX_PACKAGE_RETURN = 50
 pypi_base_url = "https://pypi.python.org/pypi/"
 
-with open("token.txt") as t:
-    bot = telebot.TeleBot(t.readline().replace('\n', ''))
+def get_token():
+    if os.path.isfile("token.txt"):
+        with open("token.txt") as t:
+            token = t.readline().replace('\n', '')
+    else:
+        token = os.environ.get('TELEGRAM_TOKEN')
+    return token
+
+def get_bot(token):
+    return telebot.TeleBot(token)
 
 with open("simple.html") as f:
     h = fromstring(f.read())
@@ -25,28 +34,37 @@ with open("simple.html") as f:
     for e in sel(h):
         packages.append(e.get("href"))
 
+token = get_token()
+bot = get_bot(token)
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(
-        message, "Hola! Estoy a tu servicio. Para una lista de comandos escribe /help, soy un bot en construccion")
+        message, "Hola! Estoy a tu servicio. "
+        "Para una lista de comandos escribe /help, soy un bot en construccion")
 
 
 @bot.message_handler(commands=['about'])
 def send_about(message):
-    bot.reply_to(message, "Hola! Soy PyVeBot!\nSoy un bot para hacer mas simple la busqueda y enlace a paquetes del repositorio PyPi!\nFui creado por @DrBomb y tengo un repositorio con mi codigo fuente en https://github.com/pyve/PyVenezuelaBot")
+    bot.reply_to(message, "Hola! Soy PyVeBot!\nSoy un bot para hacer mas "
+                 "simple la busqueda y enlace a paquetes del repositorio "
+                 "PyPi!\nFui creado por @DrBomb y mantenido por Python "
+                 "Venezuela tengo un repositorio con mi codigo fuente en "
+                 "https://github.com/pyve/PyVenezuelaBot")
 
 
 @bot.message_handler(commands=["help"])
 def send_help(message):
     bot.reply_to(
-        message, "/help para esta ayuda\n/pypi para link directo\n/pysearch para busqueda de paquete\n/about sobre este bot")
+        message, "/help para esta ayuda\n/pypi para link directo\n/pysearch "
+        "para busqueda de paquete\n/about sobre este bot")
 
 
 @bot.message_handler(commands=['pypi'])
 def pypi(message):
     argument = get_arg(message.text)
-    if argument == '' or argument == None:
+    if argument == '' or argument is None:
         return
     bot.reply_to(message, locate_or_list(argument))
 
@@ -54,7 +72,7 @@ def pypi(message):
 @bot.message_handler(commands=['pysearch'])
 def pysearch(message):
     argument = get_arg(message.text)
-    if argument == '' or argument == None:
+    if argument == '' or argument is None:
         return
     response = list_packages(argument)
     splitted_text = util.split_string(response, 3000)
