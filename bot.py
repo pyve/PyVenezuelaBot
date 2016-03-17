a800ac7dbb58fdc3e55a64ec3b8ba1712040ee71
Nhomar Hernández <nhomar@vauxoo.com>
2016-03-16 23:09:06 -0600

    [IMP] Autopep8. [IMP] Correcto encabezado para codificar el archivo y correrlo con el python del entorno. [IMP] Doc minima en el script.

--- bot.py
+++ bot.py
@@ -1,4 +1,12 @@
-import telebot, random
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Este es un bot para buscar en pypi por ti cualquier paquete python.
+usado en el grupo python Venezuela en telegram.
+"""
+
+import telebot
+import random
 from telebot import util
 from lxml.etree import fromstring
 from lxml.cssselect import CSSSelector
@@ -17,24 +25,31 @@ with open("simple.html") as f:
     for e in sel(h):
         packages.append(e.get("href"))
 
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Hola! Estoy a tu servicio. Para una lista de comandos escribe /help, soy un bot en construccion")
+    bot.reply_to(
+        message, "Hola! Estoy a tu servicio. Para una lista de comandos escribe /help, soy un bot en construccion")
+
 
 @bot.message_handler(commands=['about'])
 def send_about(message):
     bot.reply_to(message, "Hola! Soy PyVeBot!\nSoy un bot para hacer mas simple la busqueda y enlace a paquetes del repositorio PyPi!\nFui creado por @DrBomb y tengo un repositorio con mi codigo fuente en https://github.com/pyve/PyVenezuelaBot")
 
+
 @bot.message_handler(commands=["help"])
 def send_help(message):
-    bot.reply_to(message,"/help para esta ayuda\n/pypi para link directo\n/pysearch para busqueda de paquete\n/about sobre este bot")
+    bot.reply_to(
+        message, "/help para esta ayuda\n/pypi para link directo\n/pysearch para busqueda de paquete\n/about sobre este bot")
+
 
 @bot.message_handler(commands=['pypi'])
 def pypi(message):
     argument = get_arg(message.text)
     if argument == '' or argument == None:
         return
-    bot.reply_to(message,locate_or_list(argument))
+    bot.reply_to(message, locate_or_list(argument))
+
 
 @bot.message_handler(commands=['pysearch'])
 def pysearch(message):
@@ -44,62 +59,70 @@ def pysearch(message):
     response = list_packages(argument)
     splitted_text = util.split_string(response, 3000)
     for text in splitted_text:
-        bot.reply_to(message,text)
+        bot.reply_to(message, text)
+
 
 def get_arg(argument):
-    regexp = re.compile("\/\w*(@\w*)*\s*([\s\S]*)",re.IGNORECASE)
+    regexp = re.compile("\/\w*(@\w*)*\s*([\s\S]*)", re.IGNORECASE)
     textmatch = regexp.match(argument)
     return textmatch.group(2)
+
 
 def list_packages(argument):
     count = 0
     results = []
-    regexp = re.compile(argument,re.IGNORECASE)
+    regexp = re.compile(argument, re.IGNORECASE)
     for x in packages:
         if regexp.search(x) is not None:
             results.append(x)
-            count+=1
-    if count==0:
+            count += 1
+    if count == 0:
         return package_not_found(argument)
     elif count > MAX_PACKAGE_RETURN:
-        return too_many_packages(argument,results,count)
+        return too_many_packages(argument, results, count)
     else:
-        return package_list(argument,results,count)
+        return package_list(argument, results, count)
+
 
 def locate_or_list(argument):
     if argument.lower() in packages:
-      return package_located(argument)
-    regexp = re.compile(argument,re.IGNORECASE)
+        return package_located(argument)
+    regexp = re.compile(argument, re.IGNORECASE)
     count = 0
     results = []
     for x in packages:
         if regexp.search(x) is not None:
             results.append(x)
-            count+=1
-    if count==0:
+            count += 1
+    if count == 0:
         return package_not_found(argument)
     elif count > MAX_PACKAGE_RETURN:
-        return too_many_packages(argument,results,count)
+        return too_many_packages(argument, results, count)
     else:
-        return package_list(argument,results,count)
+        return package_list(argument, results, count)
+
 
 def package_located(argument):
     return pypi_base_url + argument
 
+
 def package_not_found(argument):
     return "Su busqueda regreso 0 resultados"
 
-def package_list(argument,results,count):
-    response = "{} Resultados:\n".format(count) if count>1 else "{} Resultado:\n".format(count)
+
+def package_list(argument, results, count):
+    response = "{} Resultados:\n".format(
+        count) if count > 1 else "{} Resultado:\n".format(count)
     for x in results:
-      response += x + "\n"
+        response += x + "\n"
     return response
 
-def too_many_packages(argument,results,count):
+
+def too_many_packages(argument, results, count):
     response = "{} Resultados:\nAlgunos de estos son:\n".format(count)
     for x in range(MAX_PACKAGE_RETURN):
-      response += random.choice(results) + "\n"
+        response += random.choice(results) + "\n"
     return response
 
-if __name__=="__main__":
+if __name__ == "__main__":
     bot.polling()
